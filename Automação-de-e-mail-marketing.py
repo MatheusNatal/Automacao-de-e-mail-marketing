78ff93cd21a1933786efe3dadcbbeddf706b5d13
MatheusNatal <matheusnatal61@hotmail.com>
2023-04-01 19:41:44 -0300

    update

--- Automação-de-e-mail-marketing.py
+++ Automação-de-e-mail-marketing.py
@@ -1,44 +1,70 @@
 import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
 import schedule
 import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
+# Configurações do servidor SMTP e informações da conta de e-mail
+smtp_server = 'smtp.office365.com'
+smtp_port = 587
+smtp_username = 'carloshenrique783@hotmail.com'
+smtp_password = 'Pimenta10@'
 
-# Configuração do servidor SMTP e informações da conta
+# Informações sobre o remetente do e-mail
+de = 'carloshenrique783@hotmail.com'
+
+# Ler lista de destinatários a partir do arquivo lista.txt
+with open('lista.txt', 'r') as f:
+    para_lista = [linha.strip() for linha in f]
+
+# Crie uma mensagem de e-mail com corpo HTML
+msg = MIMEMultipart('related')
+msg['From'] = de
+msg['Subject'] = "OPORTUNIDADE ÚNICA"
+
+html = """\
+<html>
+  <body>
+    <p>Olá, <br>
+
+Você sabia que é possível ganhar dinheiro na internet sem sair de casa? Se você está procurando uma maneira de aumentar sua renda ou deseja criar uma nova fonte de renda, temos a solução perfeita para você.<br>
+<br>
+Nosso curso de marketing digital é uma oportunidade incrível para aprender as estratégias mais eficazes para ganhar dinheiro online. Com a orientação de nossos especialistas em marketing, você aprenderá como criar e promover produtos digitais, como usar o marketing de afiliados e muito mais.<br>
+<br>
+Além disso, você terá acesso a aulas práticas e exercícios para aplicar todo o conhecimento que aprendeu. E se surgirem dúvidas, nossos tutores estarão disponíveis para ajudar.<br>
+<br>
+Não perca a chance de transformar seu futuro financeiro. Inscreva-se agora em nosso curso de marketing digital e comece a ganhar dinheiro na internet.<br>
+<br>
+Atenciosamente,<br>
+Carlos Henrique, Gestor de vendas empresa piru.</p>
+    <p><a href="https://metodohomeofficelucrativo.com.br/pv/?mcr=AQM22263687">Clique aqui</a>e mude de vida agora mesmo!</p>
+  </body>
+</html>
+"""
+
+# Função para enviar o e-mail
 def enviar_email():
-    with open('emails.txt', 'r') as arquivo:
-        lista_emails = arquivo.read().splitlines()
+    for para in para_lista:
+        # Crie um novo objeto MIMEMultipart para cada destinatário
+        msg_individual = MIMEMultipart()
+        msg_individual['To'] = para
+        msg_individual['Subject'] = 'Oportunidade Única'
+        msg_individual.attach(MIMEText(html, 'html'))
+        '''msg_individual.attach(img)'''
 
-    server = smtplib.SMTP('smtp.office365.com', 587)
-    server.starttls()
-    server.login("seu_email@example.com", "sua_senha")
+        # Conecte-se ao servidor SMTP e envie o e-mail
+        smtp = smtplib.SMTP(smtp_server, smtp_port)
+        smtp.starttls()
+        smtp.login(smtp_username, smtp_password)
+        smtp.sendmail(de, para, msg_individual.as_string())
+        smtp.quit()
+        print(f'E-mail enviado com sucesso para {para}!')
 
-    # Criação da mensagem
-    msg = MIMEMultipart()
-    msg['From'] = "seu_email@example.com"
-    destinatarios = lista_emails
-    msg['Subject'] = "Assunto do E-mail"
+# Agende o envio do e-mail para as 10:30 todos os dias
+schedule.every().day.at("10:06").do(enviar_email)
 
-    # Adicionando o corpo da mensagem
-    corpo = MIMEText("Corpo do E-mail")
-    msg.attach(corpo)
-
-    # Adicionando uma imagem ao e-mail
-    with open("imagem.jpg", "rb") as f:
-        imagem = MIMEImage(f.read())
-        msg.attach(imagem)
-
-    # Enviando a mensagem
-    server.sendmail("seu_email@example.com", ", ".join(destinatarios), msg.as_string())
-    server.quit()
-
-# Configurar o agendamento de envio de e-mails
-schedule.every().day.at("09:00").do(enviar_email)  # Envio diário às 09:00
-schedule.every().day.at("14:30").do(enviar_email)  # Envio diário às 14:30
-
-# Loop principal para executar o agendamento
+# Loop principal para executar as tarefas agendadas
 while True:
     schedule.run_pending()
-    time.sleep(60)  # Esperar 1 minuto antes de verificar novamente
+    time.sleep(1)
